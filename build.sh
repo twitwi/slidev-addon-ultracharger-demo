34d26b8d769f15f45dc323be56985cc0f3143cd5
Rémi Emonet <remi.emonet@reverse--com.heeere>
2022-12-07 00:19:44 +0100

    Build script for the demo gh-pages

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+alert() {
+    echo "#### $@"
+}
+
+repo=$(git remote get-url origin | sed -e 's@.*/@@g' -e 's@\.git$@@g')
+git branch --show-current | grep main
+git diff HEAD > ",,save-diff-$(date --iso-8601=seconds)"
+
+
+rm -rf dist/
+slidev build --base=/"$repo"
+git switch gh-pages
+{
+    cp -rf dist/* .
+    git add .
+    git commit -m "Scripted build for gh-pages on $(date --iso-8601=seconds)"
+    git push
+    git switch main
+} || {
+    alert "NB: Cannot commit and push... switching back to main"
+    git switch main
+}
+alert ALL OK
+
