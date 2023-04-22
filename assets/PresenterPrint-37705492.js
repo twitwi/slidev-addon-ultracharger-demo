e588cbc24430f41224790a72e3a51e1ea38118d7
Rémi Emonet <remi.emonet@reverse--com.heeere>
2023-04-23 01:14:08 +0200

    Scripted build for gh-pages on 2023-04-23T01:14:08+02:00

---
+++ assets/PresenterPrint-37705492.js
@@ -0,0 +1,17 @@
+import{h as d,j as _,k as p,ak as h,c as m,al as u,m as n,am as t,an as o,z as s,F as f,ao as g,ap as v,aq as x,q as l,ar as y,as as b,n as k,at as N,au as j,_ as w}from"./nav-25e95d2e.js";import{N as P}from"./NoteDisplay-c25ff256.js";import{u as S}from"./index-5e234b4a.js";const V={class:"m-4"},L={class:"mb-10"},T={class:"text-4xl font-bold mt-2"},z={class:"opacity-50"},B={class:"text-lg"},C={class:"font-bold flex gap-2"},D={class:"opacity-50"},H=t("div",{class:"flex-auto"},null,-1),q={key:0,class:"border-gray-400/50 mb-8"},F=d({__name:"PresenterPrint",setup(M){_(p),h(`
+@page {
+  size: A4;
+  margin-top: 1.5cm;
+  margin-bottom: 1cm;
+}
+* {
+  -webkit-print-color-adjust: exact;
+}
+html,
+html body,
+html #app,
+html #page-root {
+  height: auto;
+  overflow: auto !important;
+}
+`),S({title:`Notes - ${m.title}`});const i=u(()=>x.slice(0,-1).map(a=>{var r;return(r=a.meta)==null?void 0:r.slide}).filter(a=>a!==void 0&&a.noteHTML!==""));return(a,r)=>(l(),n("div",{id:"page-root",style:v(s(j))},[t("div",V,[t("div",L,[t("h1",T,o(s(m).title),1),t("div",z,o(new Date().toLocaleString()),1)]),(l(!0),n(f,null,g(s(i),(e,c)=>(l(),n("div",{key:c,class:"flex flex-col gap-4 break-inside-avoid-page"},[t("div",null,[t("h2",B,[t("div",C,[t("div",D,o(e==null?void 0:e.no)+"/"+o(s(y)),1),b(" "+o(e==null?void 0:e.title)+" ",1),H])]),k(P,{"note-html":e.noteHTML,class:"max-w-full"},null,8,["note-html"])]),c<s(i).length-1?(l(),n("hr",q)):N("v-if",!0)]))),128))])],4))}}),W=w(F,[["__file","/home/twilight/projects/deck-js-stuffs/slidev-addon-ultracharger-demo/node_modules/.pnpm/@slidev+client@0.40.3_vite@4.1.1/node_modules/@slidev/client/internals/PresenterPrint.vue"]]);export{W as default};
