09d25d4e7f9e6801a749c05b7c2066de46e1520c
Rémi Emonet <remi.emonet@reverse--com.heeere>
2023-11-20 11:34:06 +0100

    Better feature splash (bigger text + background in case of no network)

--- setup/preparser.ts
+++ setup/preparser.ts
@@ -15,8 +15,8 @@ function match(l: String, reg: RegExp): any {
 
 const FEATURE = (name, frontmatter) => `
 ---
-background: https://source.unsplash.com/JgOeRuGD_Y4/medium
 layout: cover
+class: FEATURE
 attibution: Photo by <a href="https://unsplash.com/@heytowner?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">JOHN TOWNER</a> on <a href="https://unsplash.com/s/photos/dark-background?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
 ---
 # NEXT FEATURE
